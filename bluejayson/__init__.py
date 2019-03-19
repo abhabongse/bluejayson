fc64f29ae785c96e9713d656d6a69b8d80297656
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2019-03-20 00:39:14 +0700

    Create python package

--- bluejayson/__init__.py
+++ bluejayson/__init__.py
@@ -3,13 +3,7 @@ BlueJayson is a simple Pythonic data definition library to handle very
 basic data serializations and validations, and it works with JSON data.
 """
 
-__author__ = "Abhabongse Janthong"
-__license__ = "Apache License 2.0"
-__version__ = "0.1.0"
-__maintainer__ = "Abhabongse Janthong"
-__email__ = "6845502+abhabongse@users.noreply.github.com"
-__status__ = "Development"
-
+from bluejayson.metadata import __author__, __license__, __version__, __maintainer__, __email__, __status__
 from bluejayson.schema import BaseSchema
 
 __all__ = [
