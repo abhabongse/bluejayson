d3226ba44d40627c119378aba0dbef975e15b2f0
Abhabongse Janthong <abhabongse.j@kbtg.tech>
2019-03-20 12:40:13 +0700

    Introduce dev-release & Add PyPI project keywords

--- bluejayson/__init__.py
+++ bluejayson/__init__.py
@@ -3,7 +3,7 @@ BlueJayson is a simple Pythonic data definition library to handle very
 basic data serializations and validations, and it works with JSON data.
 """
 
-from bluejayson.metadata import __author__, __license__, __version__, __maintainer__, __email__, __status__
+from bluejayson.__metadata__ import __author__, __license__, __version__, __maintainer__, __email__, __status__
 from bluejayson.schema import BaseSchema
 
 __all__ = [
