1fa7da2b1c3a396d97fa75d7ab225f0334f864a3
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-12-11 15:26:44 +0700

    Move meta.json file to package root

--- src/bluejayson/__init__.py
+++ src/bluejayson/__init__.py
@@ -10,7 +10,7 @@ import os
 __all__ = []
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
-metadata_file = os.path.join(this_dir, 'legacy/meta.json')
+metadata_file = os.path.join(this_dir, 'meta.json')
 
 try:
     with open(metadata_file) as fobj:
