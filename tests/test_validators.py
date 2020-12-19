defef644c7803bb1a095855811f505f69e323fdd
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-12-20 01:02:34 +0700

    Add unit tests for Range validator

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pytest  # noqa
 
-from bluejayson.validators import Equal, Predicate, ValidationFailed
+from bluejayson.validators import Equal, Predicate, Range, ValidationFailed
 
 
 @pytest.mark.parametrize("validator,value", [
@@ -13,6 +13,18 @@ from bluejayson.validators import Equal, Predicate, ValidationFailed
     (Equal(0j), 0.00),
     (Equal("none"), "none"),
     (Equal({0, 1, 2}), {n % 3 for n in range(10)}),
+    (Range(min=7), 10),
+    (Range(max=-3), -5),
+    (Range(min=-12, max=-6), -10),
+    (Range(min=-12, max=-6), -12),
+    (Range(min=-12, max=-6), -6),
+    (Range(min=-12, max=-6, min_inclusive=False), -10),
+    (Range(min=-12, max=-6, min_inclusive=False), -6),
+    (Range(min=-12, max=-6, max_inclusive=False), -10),
+    (Range(min=-12, max=-6, max_inclusive=False), -12),
+    (Range(min=-12, max=-6, min_inclusive=False, max_inclusive=False), -10),
+    (Range(min="elephant", max="hippo"), "giraffe"),
+    (Range(min=(3, 10), max=(3, 11)), (3, 10, None)),
 ])
 def test_validator_pass(validator, value):
     assert validator.validate(value)
@@ -27,6 +39,16 @@ def test_validator_pass(validator, value):
     (Equal(0j), 1.e-100, 'not_matched'),
     (Equal(float("nan")), float("nan"), 'not_matched'),
     (Equal({1, 2, 3}), {1, 3, 5}, 'not_matched'),
+    (Range(min=7), 4, 'out_of_range'),
+    (Range(max=-3), 7, 'out_of_range'),
+    (Range(min=-12, max=-6), -24, 'out_of_range'),
+    (Range(min=-12, max=-6), 99, 'out_of_range'),
+    (Range(min=-12, max=-6, min_inclusive=False), -12, 'out_of_range'),
+    (Range(min=-12, max=-6, max_inclusive=False), -6, 'out_of_range'),
+    (Range(min=-12, max=-6, min_inclusive=False, max_inclusive=False), -12, 'out_of_range'),
+    (Range(min=-12, max=-6, min_inclusive=False, max_inclusive=False), -16, 'out_of_range'),
+    (Range(min=-12, max=-6), "colony", 'incomparable'),
+    (Range(min=(1,)), 2, 'incomparable'),
 ])
 def test_validator_failed(validator, value, error_code):
     with pytest.raises(ValidationFailed) as exc_info:
@@ -38,6 +60,8 @@ def test_validator_failed(validator, value, error_code):
 @pytest.mark.parametrize("validator_constructor,exc_cls", [
     (lambda: Predicate(lambda: None), TypeError),
     (lambda: Predicate(lambda _x, _y: None), TypeError),
+    (lambda: Range(min=0, min_inclusive=1), TypeError),
+    (lambda: Range(max=100, max_inclusive=1), TypeError),
 ])
 def test_validator_setup_error(validator_constructor, exc_cls):
     with pytest.raises(exc_cls):
@@ -47,6 +71,8 @@ def test_validator_setup_error(validator_constructor, exc_cls):
 @pytest.mark.parametrize("validator,value,exc_cls", [
     (Predicate(lambda x: x >= 0), "hello", TypeError),
     (Predicate(lambda x: x), "hello", TypeError),
+    (Range(min=-12, max=-6, absorb_cmp_error=False), "colony", TypeError),
+    (Range(min=(1,), absorb_cmp_error=False), 2, TypeError),
 ])
 def test_validator_runtime_error(validator, value, exc_cls):
     with pytest.raises(exc_cls):
